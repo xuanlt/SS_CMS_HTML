d1548af9619801cfd3a6cfdf1f12b102576cf4ea
xuanlt <xuanlt@live.com>
2023-01-05 13:47:00 +0700

    Update sidebar
    Fixed Submenu sidebar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,87 @@
 jQuery(document).ready(function($) {
 
     //Function layout
-    function layout() {
+    function sidebar_layout() {
+        let window_height = $(window).outerHeight();
 
+        //Sidebar 
+        let sidebar_header_height = $('.site-sidebar .sidebar-header').outerHeight();
+        let sidebar_footer_height = $('.site-sidebar .sidebar-footer').outerHeight();
+
+        console.log(sidebar_footer_height);
+
+        $('.site-sidebar .sidebar-content').css('height', window_height - sidebar_header_height - sidebar_footer_height + 'px');
+        submenu_sidebar_collapsed();
     }
 
-    layout();
+    sidebar_layout();
+    // Sidebar
+    if (($(window).width() < 540) && ($('body').hasClass('.sidebar-collapsed') == false)) {
+        $('.btn-sidebar-toggle').click();
+    }
+
+    //Sub Menu when Sidebar Collaped
+    function submenu_sidebar_collapsed() {
+
+        if ($('.sidebar-collapsed').length != 0) {
+            $('.sidebar-nav .nav-item').each(function() {
+                let window_height = $(window).height();
+                let el_this_height = $(this).height();
+                let el_menu_height = $(this).find('.nav-sub-menu').outerHeight();
+                let el_offet_top = $(this).offset().top;
+                let el_offet_bottom = window_height - $(this).offset().top - el_this_height;
+
+                if (el_menu_height > el_offet_bottom) {
+                    $(this).find('.nav-sub-menu').css({
+                        'top': 'auto',
+                        'bottom': el_offet_bottom
+                    });
+                } else {
+                    $(this).find('.nav-sub-menu').css({
+                        'top': el_offet_top,
+                        'bottom': 'auto'
+                    });
+                }
+            });
+
+            $('.tree .treeview').each(function() {
+                let window_height = $(window).height();
+                let el_this_height = $(this).height();
+                let el_menu_height = $(this).find('.treeview-menu').outerHeight();
+                let el_offet_top = $(this).offset().top;
+                let el_offet_bottom = window_height - $(this).offset().top - el_this_height;
+
+                if (el_menu_height > el_offet_bottom) {
+                    $(this).find('.treeview-menu').css({
+                        'top': 'auto',
+                        'bottom': el_offet_bottom
+                    });
+                } else {
+                    $(this).find('.treeview-menu').css({
+                        'top': el_offet_top,
+                        'bottom': 'auto'
+                    });
+                }
+
+            });
+        }
+    }
+
+    //Site Sidebar Scroll
+    $(function() {
+        $(".sidebar-content").niceScroll({
+            cursorcolor: 'rgba(255,255,255,0.2)',
+            cursorwidth: '2px',
+            cursorborder: 'none',
+            railalign: 'left'
+        });
+    });
+
+    $(".sidebar-content").scroll(function() {
+        submenu_sidebar_collapsed();
+    });
+
+
 
     //Sidebar Toggle 
     $(".item-sidebar-toggle > .nav-link, .btn-sidebar-toggle").click(function(e) {
@@ -13,14 +89,11 @@ jQuery(document).ready(function($) {
         $(".item-sidebar-toggle > .nav-link, .btn-sidebar-toggle").toggleClass('active');
         $(".item-sidebar-toggle > .nav-link, .btn-sidebar-toggle").find('.fa-solid').toggleClass('fa-bars fa-bars-staggered');
         $('body').toggleClass('sidebar-collapsed');
+        sidebar_layout();
     });
 
-    // Side Bar First load
-    if (($(window).width() < 540) && ($('body').hasClass('.sidebar-collapsed') == false)) {
-        $('.btn-sidebar-toggle').click();
-    }
 
-    //Sidebar Toggle
+    //Sub Menu Toggle
     $('.tree .treeview > a').click(function(e) {
         e.preventDefault();
         //Open
@@ -29,11 +102,10 @@ jQuery(document).ready(function($) {
         //Close
         $('.tree .treeview > a').not($(this)).parent().removeClass('menu-opened active');
         $('.tree .treeview').not($(this)).find('.treeview-menu').hide();
-    })
 
-    // Noticaiton
+    });
 
-    //Setup Nicescroll
+    // Setup Nicescroll
     $(function() {
         $(".nicescroll").niceScroll({
             cursorcolor: 'rgba(0,0,0,0.2)',
@@ -44,6 +116,6 @@ jQuery(document).ready(function($) {
 
     //Window resize
     $(window).resize(function() {
-        layout();
+        sidebar_layout();
     });
 });
